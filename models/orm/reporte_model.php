040c96e33d69d8fdca33d0a9d5b2cfbbe9cd57c4
Elioth Rivera <elioth010@elioth010.elioth010>
2016-10-05 20:29:44 -0600

    cambios a reportes

--- models/orm/reporte_model.php
+++ models/orm/reporte_model.php
@@ -8,34 +8,6 @@
 
 		}
 
-		public function guardar(){
-			$info = json_decode($_POST['info']);
-			$data = array(
-				'id'=>'',
-				'habitacion' => $info->habitacion,
-				'fecha_inicio' => $info->fecha_inicio,
-				'fecha_fin' => $info->fecha_fin,
-				'precio_normal' => $info->precio_normal,
-				'precio_nocturno' => $info->precio_nocturno,
-				'estado'=>1);
-
-
-			$promocion = new promocion_orm($data);
-			try {
-				$result = $promocion->save();
-				if($result == null){
-					header("HTTP/1.1 505 Internal Error");
-					echo json_encode("no se pudo insertar el registro");
-					return;
-				}
-				echo json_encode($result);
-			} catch (Exception $e) {
-				echo json_encode($e->getMessage());
-			}
-		}
-
-
-
 		public function traer_habitaciones(){
 			$habitaciones = habitacion_orm::where('estado', 1);
 
@@ -55,7 +27,9 @@
 		public function llenar_tabla(){
 			$info = json_decode($_POST['info']);
 			$general = new general_orm();
-			$reportes = $general::query("SELECT * FROM transaccion WHERE hora_inicio >='".$info->fecha_inicio."' AND hora_salida <='".$info->fecha_fin." AND habitacion");
+			$date = date('Y-m-d', strtotime($info->fecha_inicio));
+			$date2 = date('Y-m-d', strtotime($info->fecha_fin));
+			$reportes = $general::query("SELECT * FROM transaccion WHERE hora_inicio >='".$date." 00:00:00' AND hora_salida <='".$date2." 23:59:59' AND habitacion");
 
 			$tabla = '<table id="reporte" class="display" cellspacing="0" width="100%">
 					<thead>
@@ -96,18 +70,6 @@
 			$tabla = $tabla.'</tbody>
 				</table>';
 			echo $tabla;
-		}
-
-		public function traer_dato(){
-			$info = json_decode($_POST['info']);
-
-			$promocion = promocion_orm::where("id", $info->id);
-
-
-
-			$result = array('cod' => 1, 'datos' => $promocion);
-
-			echo json_encode($result);
 		}
 
 	}
